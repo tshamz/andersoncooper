11ba2ae1f5dab7bef433680556666e720d1d967a
Tyler Shambora <tyler@theshamboras.com>
2016-05-13 12:23:32 -0700

    wip

--- responses.js
+++ responses.js
@@ -47,16 +47,12 @@ module.exports = {
               });
             });
           } else {
-            setTimeout(function() {
-              convo.say('Great! Moving forward...');
-              console.log('https://twitter.com/bvatweetbot/status/' + parsedBody.id_str);
-              // bot.say({
-              //   channel: '#tweet-game-on-fleek',
-              //   username: 'Anderson Cooper: Keeper of the Tweets',
-              //   icon_url: 'http://dev.tylershambora.com/images/anderson-pooper.jpg',
-              //   text: 'Hey! Checkout what '+ userName +' just tweeted!\n' + 'https://twitter.com/bvatweetbot/status/' + parsedBody.id
-              // });
-            }, 2000);
+            bot.say({
+              channel: '#tweet-game-on-fleek',
+              username: 'Anderson Cooper: Keeper of the Tweets',
+              icon_url: 'http://dev.tylershambora.com/images/anderson-pooper.jpg',
+              text: 'Hey! Checkout what '+ userName +' just tweeted!\n' + 'https://twitter.com/bvatweetbot/status/' + parsedBody.id_str
+            });
           }
         });
         convo.next();
