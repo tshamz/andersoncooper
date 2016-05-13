2ad19ecca8f09a52a96411b1cb8e5557fe80fc0c
Tyler Shambora <tyler@theshamboras.com>
2016-05-13 10:11:56 -0700

    wip

--- andersoncooper.js
+++ andersoncooper.js
@@ -121,8 +121,7 @@ controller.hears([/post to twitter ([\s\S]*)/], ['direct_message'], function(bot
     convo.say("I'm going to try and tweet this.");
 
     request.post({url: resourceUrl, oauth: oauth, qs: queryString}, function(error, response, body) {
-      console.log(typeof body);
-      console.log(body.hasOwnProperty('errors'));
+      body = JSON.parse(body);
       if (body.hasOwnProperty('errors')) {
         console.log('ding');
         body.errors.forEach(function(error) {
