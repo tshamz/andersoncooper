158cb91daa84804ee550f0daa064f90dd4a08426
Tyler Shambora <tyler@theshamboras.com>
2016-05-13 17:48:02 -0700

    fin.

--- andersoncooper.js
+++ andersoncooper.js
@@ -6,7 +6,7 @@ var Botkit               = require('botkit');
 var emoji                = require('node-emoji');
 var responses            = require('./responses.js');
 
-var blacklistedUsers     = ['tyler shambora'];
+var blacklistedUsers     = ['tyler shambora', 'niiki johnson'];
 var readOnlyChannels     = [];
 
 
